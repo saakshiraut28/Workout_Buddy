b6fb3b176d23ac7caa9724b022e594c33546c590
Saakshi Raut <saakshiraut28@gmail.com>
2024-01-07 00:17:02 +0530

    Delete workout now working

--- frontend/src/components/workoutDetails.js
+++ frontend/src/components/workoutDetails.js
@@ -1,6 +1,20 @@
 /** @format */
 
 const WorkoutDetails = ({ workout }) => {
+  const handleDelete = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = response.json();
+
+    if (response.ok) {
+      console.log("Workout deleted");
+      window.location.reload(true);
+    }
+    if (!response.ok) {
+      console.log(response);
+    }
+  };
   return (
     <div className="Details grid bg-white border px-5 md:px-10 space-y-1 py-5 my-4">
       <p className="text-md font-bold text-[#1fb84e]">{workout.title}</p>
@@ -11,6 +25,7 @@ const WorkoutDetails = ({ workout }) => {
       <p className="text-sm font-regular text-gray-500">
         Created At: {workout.createdAt}
       </p>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
