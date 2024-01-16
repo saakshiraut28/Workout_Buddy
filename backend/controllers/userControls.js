7edbb6d84b74141053357eeab931b1d16ec34335
Saakshi Raut <saakshiraut28@gmail.com>
2024-01-17 02:01:59 +0530

    Sign Up backend logic complete

--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -1,11 +1,20 @@
 /** @format */
 
+const User = require("../models/userSchema");
+
 const loginUser = (req, res) => {
   res.status(200).json({ msg: "Login User" });
 };
 
-const signupUser = (req, res) => {
-  res.status(200).json({ msg: "Sign Up User" });
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signUp(email, password);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
