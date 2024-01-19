cdca63ec4daadd8289cab87315eec1a8db8d5c87
Saakshi Raut <saakshiraut28@gmail.com>
2024-01-20 00:37:44 +0530

    Create backend logic for login functionality

--- backend/controllers/userControls.js
+++ backend/controllers/userControls.js
@@ -7,8 +7,17 @@ const createToken = (id) => {
   return jwt.sign({ id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
-const loginUser = (req, res) => {
-  res.status(200).json({ msg: "Login User" });
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.loginUser(email, password);
+    // Create token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const signupUser = async (req, res) => {
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -21,26 +21,46 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+// static method for login
+
+userSchema.statics.loginUser = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("User does not exist");
+  }
+
+  const match = bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect credentials");
+  }
+  return user;
+};
+
 // static method for signup
 
 userSchema.statics.signUp = async function (email, password) {
   if (!email || !password) {
-    throw Error("All fields must be filled.");
+    throw Error("All fields must be filled");
   }
 
   if (!validator.isEmail(email)) {
-    throw Error("Enter valid Email address.");
+    throw Error("Enter valid Email address");
   }
 
   if (!validator.isStrongPassword(password)) {
-    throw Error("Enter a strong password.");
+    throw Error("Enter a strong password");
   }
 
   // Check if the user exist
   const exists = await this.findOne({ email });
 
   if (exists) {
-    throw Error("Email already in use.");
+    throw Error("Email already in use");
   }
 
   const salt = await bcrypt.genSalt(10);
