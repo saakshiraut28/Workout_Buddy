69f7e9864c50d22b5bea8665fd3c9255c8ee6696
Saakshi Raut <saakshiraut28@gmail.com>
2024-01-28 14:32:24 +0530

    Add exact to the home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
